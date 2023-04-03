30d3c9ef8aa235df446d9f64e43e7b65415859ac
JuriJuriM <jurimudry@gmail.com>
2023-04-03 15:39:27 +0300

    created folder of header

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,3 @@
 import './index.scss';
 
-const headerBurger = document.querySelector('.header__burger');
-const headerMenu = document.querySelector('.header__menu');
-const body = document.querySelector('body');
-
-function handleClick() {
-  headerBurger.classList.toggle('active');
-  headerMenu.classList.toggle('active');
-  body.classList.toggle('lock');
-}
-
-headerBurger.addEventListener('click', handleClick);
+import '../../components/header/header';
