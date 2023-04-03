2dfe3f4d57cba89abf77e7e84d7eef1d146db0c8
JuriJuriM <jurimudry@gmail.com>
2023-04-03 12:47:28 +0300

    file js is ready!

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,14 @@
 import './index.scss';
+
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
+
+function handleClick() {
+  console.log('Click');
+  headerBurger.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  body.classList.toggle('lock');
+}
+
+headerBurger.addEventListener('click', handleClick);
